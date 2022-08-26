3cd79619d0d24256af624ae520cfad39d9a460a3
priscilafraifer <priscilafraifer@gmail.com>
2022-08-26 19:34:04 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -6,9 +6,21 @@ import './style.css';
 let list: number[] = [1, 2, 3];
 let otherList: Array<number> = [1, 2, 3];
 
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById('app1');
 appDiv.innerHTML = `
 <h1>Array declaration</h1>
 <h2>List: ${list}</h2>
 <h2>other list: ${otherList}</h2>
+`;
+
+let me: [string, number];
+me = ['Priscila', 35];
+
+const appTuple: HTMLElement = document.getElementById('app2');
+appTuple.innerHTML = `
+<h1>Tuple</h1>
+<h1>Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
+
+
+
 `;
