bfddaa0446b975ebe3521a02951ce0ce491b2583
priscilafraifer <priscilafraifer@gmail.com>
2022-08-26 19:27:23 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -2,8 +2,13 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-let list: number[] = [1,2,3];
-let otherList
+let list: number[] = [1, 2, 3];
+let otherList: Array<number> = [1, 2, 3];
+
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+<h1>Array declaration</h1>
+<h2>List: ${list}</h2>
+<h2>other list: ${otherList}</h2>
+`;
