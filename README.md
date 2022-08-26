b5b6c2f0df475fda30efd986a4db18757a3a922d
priscilafraifer <priscilafraifer@gmail.com>
2022-08-26 19:21:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-vfw8qn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-vfw8qn)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+let list: number[] = [1,2,3];
+let otherList
