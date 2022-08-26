b9db4c341025646bbb9e37af4b1521dc3cd9b389
priscilafraifer <priscilafraifer@gmail.com>
2022-08-26 19:40:53 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -20,7 +20,25 @@ const appTuple: HTMLElement = document.getElementById('app2');
 appTuple.innerHTML = `
 <h1>Tuple</h1>
 <h1>Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
+`;
 
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let color1: Color = Color.Green;
 
+enum ColorWithRef {
+  Red = 10,
+  Green = 20,
+  Blue = 30,
+}
+let color2: ColorWithRef = ColorWithRef.Blue;
 
+const appEnum: HTMLElement = document.getElementById('app3');
+appEnum.innerHTML = `
+<h1>Enumerated types</h1>
+<h2>Cor 1: ${color1}</h2>
+<h2>Cor 2: ${color2}</h2>
 `;
